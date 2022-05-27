31f1c1f2d8c6393590de0d1e47c64add33e1cd86
cocofox1902 <colas.renard1902@gmail.com>
2022-05-27 09:51:46 +0000

    add task 3

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,22 @@
+$(document).ready(function  () {
+  const amencheck = {};
+  $('.amenities .popover input').change(function () {
+    if ($(this).is(':checked')) {
+      amencheck[$(this).attr('data-name')] = $(this).attr('data-id');
+    } else if ($(this).is(':not(:checked)')) {
+      delete amencheck[$(this).attr('data-name')];
+    }
+    $('.amenities h4').text(Object.keys(amencheck).sort().join(', '));
+  });
+});
+
+$(document).ready(function  () {
+  $.get('http://127.0.0.1:5001/api/v1/status/', function(data) {
+    if (data.status == 'OK') {
+      $('#api_status').addClass('available');
+    }
+    else {
+      $('#api_status').removeClass('available');
+    }
+  });
+});
