1ff8c3e7901564fc4146d6ab3bd38be674dea529
cocofox1902 <colas.renard1902@gmail.com>
2022-05-27 10:32:56 +0000

    add task 4

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -20,3 +20,38 @@ $(document).ready(function  () {
     }
   });
 });
+
+$.ajax({
+  type: 'POST',
+  url: 'http://127.0.0.1:5001/api/v1/places_search/',
+  data: '{}',
+  dataType: 'json',
+  contentType: 'application/json',
+  success: function (places) {
+    places.forEach(place => {
+      const guest = (place.max_guest !== 1) ? 's' : '';
+      const rooms = (place.number_rooms !== 1) ? 's' : '';
+      const bathrooms = (place.number_bathrooms !== 1) ? 's' : '';
+      const html = `<article>
+      <div class="title_box">
+        <h2>${place.name}</h2>
+        <div class="price_by_night">$${place.price_by_night}</div>
+      </div>
+      <div class="information">
+        <div class="max_guest">${place.max_guest} Guest${guest}</div>
+        <div class="number_rooms">${place.number_rooms}
+          Bedroom${rooms}</div>
+        <div class="number_bathrooms">${place.number_bathrooms}
+          Bathroom${bathrooms}</div>
+      </div>
+      <div class="user">
+        
+      </div>
+      <div class="description">
+        ${place.description}
+      </div>
+    </article>`;
+      $('.places').append(html);
+    });
+  }
+});
